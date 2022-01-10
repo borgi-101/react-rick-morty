866f352b57aa745c3d9552d83af0fdc6e4254135
Franco Borgiani <contact@franborgiani.com>
2022-01-10 20:10:25 -0300

    Initial config

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const rulesJavaScript = {
+    test: /\.(js|jsx)$/,
+    exclude: node_modules,
+    use: {
+        loader: "babel-loader"
+    }
+}
+const rulesForStyles = {
+    test: /\.s?css$/,
+    use: [
+        MiniCssExtractPlugin.loader, "css-loader", "sass-loader"
+    ]
+}
+const rulesHTML = {
+    test: /\.html$/,
+    use: {
+        loader: "html-loader"
+    }
+}
+
+
+module.exports = {
+    entry : "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "./main.js"
+    },
+    mode: "development",
+    resolve:{
+        extensions: [".js",".jsx"]
+    },
+    module: {
+        rules: [rulesHTML, rulesJavaScript, rulesForStyles]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
